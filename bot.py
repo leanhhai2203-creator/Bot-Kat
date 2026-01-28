79573b0c2485b8655209a7c93b960bb896378ce0
leanhhai2203-creator <leanhhai2203@gmail.com>
2026-01-28 07:23:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -381,9 +381,9 @@ BOSS_CONFIG = {
         "desc": "Thượng cổ Ma Thần, sức mạnh đủ để hủy thiên diệt địa."
     },
     "Thôn Thiên Kình Ma": {
-        "power_required": 300000, # Yêu cầu lực chiến cao hơn Mục Dã Di
+        "power_required": 350000, # Yêu cầu lực chiến cao hơn Mục Dã Di
         "reward": (35, 45),       # Phần thưởng hậu hĩnh hơn
-        "penalty": 8000,          # Thất bại sẽ tổn thất nặng hơn
+        "penalty": 10000,          # Thất bại sẽ tổn thất nặng hơn
         "color": 0x2ecc71,        # Màu xanh bích rực rỡ
         "desc": "Hung thú dưới đáy biển sâu, máu của nó có thể nhuộm đỏ cả đại dương."
     }
@@ -3151,6 +3151,7 @@ async def shop(interaction: discord.Interaction):
 keep_alive()
 token = os.getenv("DISCORD_TOKEN")
 bot.run(token)
+
 
 
 
